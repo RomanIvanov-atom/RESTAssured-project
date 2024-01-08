3a6e0d68059b656297754b26616e00922c683370
Roman Ivanov <atom10121998@mail.ru>
2024-01-08 19:00:58 +0300

    First try to write API tests (without Specifications)

---
+++ src/test/java/api/ReqresTest.java
@@ -0,0 +1,36 @@
+package api;
+
+import io.restassured.http.ContentType;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+
+public class ReqresTest {
+    private final static String URL = "https://reqres.in/";
+
+    @Test
+    public void checkAvatarAndIdTest() {
+
+        List<UserData> users = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get(URL + "api/users?page=2")
+                .then().log().all()
+                .extract().body().jsonPath().getList("data", UserData.class);
+
+        users.forEach(x -> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
+
+        Assert.assertTrue(users.stream().allMatch(x -> x.getEmail().endsWith("@reqres.in")));
+
+        List<String> avatars = users.stream().map(UserData::getAvatar).toList();
+        List<String> ids = users.stream().map(x -> x.getId().toString()).toList();
+
+        for (int i = 0; i < avatars.size(); i++){
+            Assert.assertTrue(avatars.get(i).contains(ids.get(i)));
+        }
+    }
+}
